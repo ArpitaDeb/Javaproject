c2ee6a00ba5c9316a1b8075709515d2510059287
arpita deb <arpitadeb1990@gmail.com>
2019-05-01 15:33:50 -0600

    starting tomcat

--- src/main/java/com/bookstore/config/SecurityConfig.java
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.bookstore.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,52 +13,58 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.bookstore.service.impl.UserSecurityService;
+import com.bookstore.service.UserService;
 import com.bookstore.utility.SecurityUtility;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private UserSecurityService userSecurityService;
-	
+
 	private BCryptPasswordEncoder passwordEncoder() {
 		return SecurityUtility.passwordEncoder();
 	}
-	
+
 	private static final String[] PUBLIC_MATCHERS = {
 			"/css/**",
 			"/js/**",
 			"/image/**",
 			"/",
-			"/myAccount"
+			"/newUser",
+			"/forgetPassword",
+			"/login",
+			"/fonts/**"
 	};
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests().
-		/*	antMatchers("/**").*/
-			antMatchers(PUBLIC_MATCHERS).
-			permitAll().anyRequest().authenticated();
-		
+				.authorizeRequests().
+				/*	antMatchers("/**").*/
+						antMatchers(PUBLIC_MATCHERS).
+				permitAll().anyRequest().authenticated();
+
 		http
-			.csrf().disable().cors().disable()
-			.formLogin().failureUrl("/login?error").defaultSuccessUrl("/")
-			.loginPage("/login").permitAll()
-			.and()
-			.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-			.logoutSuccessUrl("/?logout").deleteCookies("remember-me").permitAll()
-			.and()
-			.rememberMe();
+				.csrf().disable().cors().disable()
+				.formLogin().failureUrl("/login?error")
+				/*.defaultSuccessUrl("/")*/
+				.loginPage("/login").permitAll()
+				.and()
+				.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutSuccessUrl("/?logout").deleteCookies("remember-me").permitAll()
+				.and()
+				.rememberMe();
 	}
-	
+
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
 	}
-	
+
 }
--- src/main/java/com/bookstore/service/impl/UserSecurityService.java
+++ src/main/java/com/bookstore/service/impl/UserSecurityService.java
@@ -11,18 +11,18 @@ import com.bookstore.repository.UserRepository;
 
 @Service
 public class UserSecurityService implements UserDetailsService{
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = userRepository.findByUsername(username);
-		
+
 		if(null == user) {
 			throw new UsernameNotFoundException("Username not found");
 		}
-		
+
 		return user;
 	}
 
