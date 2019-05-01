2f9068906bd2f147b3360ac70f64949616ca11d6
arpita deb <arpitadeb1990@gmail.com>
2019-05-01 02:12:39 -0600

    login page running

---
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -0,0 +1,16 @@
+package com.bookstore;
+
+import com.bookstore.controller.HomeController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackageClasses= HomeController.class)
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
